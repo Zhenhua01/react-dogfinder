e5cffd36f83416ba13b88a5bac649d56bc3777ad
Zhenhua <zhenhua1109@yahoo.com>
2022-09-05 16:40:23 -0700

    code review

--- src/DogList.js
+++ src/DogList.js
@@ -2,21 +2,16 @@ import axios from "axios";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-function DogList() {
-
-  const [dogs, setDogs] = useState(null);
-
-  if (!dogs) {
-    axios.get('http://localhost:5001/dogs').then(res => setDogs(res.data));
-    return <p>loading</p>
-  }
+function DogList({ dogs }) {
 
   return (
     <>
       {dogs.map(({ name, src }) => (
       <div key={src}>
         <img src={`/${src}.jpg`} alt={`${name}`} height='200px'/>
+        <br/>
         <Link to={`/dogs/${name}`}>Name: {name}</Link>
+        <br/>
       </div>
     ))}
     </>
